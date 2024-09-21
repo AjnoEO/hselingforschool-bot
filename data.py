5c35e6de3fa7658bbe809d0173241160f36c73bd
Ajno <boyko.artiom@gmail.com>
2024-09-21 17:09:42 +0300

    Сбор данных из config.ini

---
+++ data.py
@@ -0,0 +1,16 @@
+import os
+from configparser import ConfigParser
+
+if os.path.exists("config.ini"):
+    __config = ConfigParser()
+    __config.read("config.ini")
+else:
+    raise FileExistsError("Отсутствует файл config.ini.\n"
+                          "Если вы клонировали или запуллили git-репозиторий, "
+                          "убедитесь, что вы скопировали example.config.ini, "
+                          "переименовали его config.ini и исправили под себя.")
+
+__data = __config["data"]
+TOKEN = __data["token"]
+OWNER_ID = int(__data["owner_id"])
+OWNER_HANDLE = __data["owner_handle"]
